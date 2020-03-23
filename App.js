1e75b74525d9ea9bf3c63b231dd60e91b689e108
Mostafabelal30 <belalmostafa1993@gmail.com>
2020-03-22 22:53:40 -0500

    beacon

--- App.js
+++ App.js
@@ -6,26 +6,16 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  View,
-} from 'react-native';
-import FlashMessage from "react-native-flash-message";
-import { Provider } from 'react-redux';
-import store from './src/redux/store'
-import Router from './src/routes/router';
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import Example from './src/Example.android';
 
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <View style={{ flex: 1 }}>
-          <Router />
-          <FlashMessage position="bottom" duration={5000} />
-        </View>
-      </Provider>
+      <View style={{flex: 1}}>
+        <Example />
+      </View>
     );
   }
 }
-
-
--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import ReduxThunk from 'redux-thunk';
-import {createStore, applyMiddleware} from 'redux';
-import reducers from '../reducers';
-
-const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
-
-export default store;
